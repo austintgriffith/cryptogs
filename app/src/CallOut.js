2341cfe5d1fc3ea07347c68b2d861d4338392dd1
Austin <austin.griffith@madwiremedia.com>
2018-02-18 08:16:22 -0700

    working call page

--- app/src/CallOut.js
+++ app/src/CallOut.js
@@ -26,7 +26,7 @@ class CallOut extends Component {
           <button type="button" className="btn btn-primary mb-2" onClick={this.props.getRad.bind(this,this.state.optional)}>Get Rad</button>
           </form>
           <div style={{padding:5,opacity:0.8}}>
-Enter an (optional) email and <b>get rad</b> with our smart contract.
+Enter an (optional) email and <b>get rad</b> with our <a target="_blank" href="https://github.com/austintgriffith/cryptogs">open source</a> <a target="_blank" href="https://ropsten.etherscan.io/address/0x6fbde1e9a910c726348b5fde68d94aac28da4d9b">smart contract</a> on Ropsten.
           </div>
       </div>
     )
